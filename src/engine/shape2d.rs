90249e6ab180a8574a97ccc28716dd4c5a5c48b3
tonis2 <tonis.anton18@gmail.com>
2019-07-28 15:28:51 +0300

    Try uniform buffers

--- src/engine/shape2d.rs
+++ src/engine/shape2d.rs
@@ -1,5 +1,6 @@
 use wgpu;
-use cgmath::{Matrix4};
+use cgmath::{Matrix4, Ortho};
+use cgmath::prelude::*;
 use crate::engine::{ShaderStage, load_glsl};
 use crate::gui::definitions::Vertex;
 
@@ -14,11 +15,9 @@ pub struct Uniforms {
     pub transform: Matrix4<f32>,
 }
 
-
 ///////////////////////////////////////////////////////////////////////////
 // Pipeline
 ///////////////////////////////////////////////////////////////////////////
-
 
 pub struct Pipeline {
    pub bind_group: wgpu::BindGroup, 
@@ -26,15 +25,70 @@ pub struct Pipeline {
 }
 
 impl Pipeline {
-    pub fn new(device: &wgpu::Device) -> Pipeline {
+    pub fn new(device: &wgpu::Device, sc_desc: &wgpu::SwapChainDescriptor) -> Pipeline {
+
+        let transform: Matrix4<f32> = Matrix4::identity();
+
+        let ortho: Matrix4<f32> = Ortho::<f32> {
+                        left: 0.0,
+                        right: sc_desc.width as f32,
+                        bottom: sc_desc.height as f32,
+                        top: 0.0,
+                        near: -1.0,
+                        far: 1.0,
+                    }
+                    .into();
+
+        let mx_ref: &[f32; 16] = ortho.as_ref();
+        let mx_ref2: &[f32; 16] = transform.as_ref();             
+
+        let uniform_buf = device
+                .create_buffer_mapped(
+                    mx_ref.len(),
+                    wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::TRANSFER_DST,
+                )
+                .fill_from_slice(mx_ref);
+
+        let uniform_buf2 = device
+            .create_buffer_mapped(
+                mx_ref2.len(),
+                wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::TRANSFER_DST,
+            )
+            .fill_from_slice(mx_ref2);                           
 
         let bind_group_layout = device.create_bind_group_layout(
-            &wgpu::BindGroupLayoutDescriptor { bindings: &[] }
+            &wgpu::BindGroupLayoutDescriptor { bindings: &[
+                wgpu::BindGroupLayoutBinding {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::VERTEX,
+                    ty: wgpu::BindingType::UniformBuffer,
+                },
+                wgpu::BindGroupLayoutBinding {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::VERTEX,
+                    ty: wgpu::BindingType::UniformBuffer,
+                },
+            ] }
         );
 
         let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
             layout: &bind_group_layout,
-            bindings: &[],
+            bindings: &[
+                wgpu::Binding {
+                    binding: 0,
+                    resource: wgpu::BindingResource::Buffer {
+                        buffer: &uniform_buf,
+                        range: 0 .. 64,
+                    },
+                },
+                wgpu::Binding {
+                    binding: 0,
+                    resource: wgpu::BindingResource::Buffer {
+                        buffer: &uniform_buf2,
+                        range: 0 .. 64,
+                    },
+                },
+            ],
         });
 
         let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
