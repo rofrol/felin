5f3077d7be9082f6d4f835e15077495926aacc13
tonis2 <tonis.anton18@gmail.com>
2019-08-03 18:24:22 +0300

    Minor edit

--- src/engine/shape2d.rs
+++ src/engine/shape2d.rs
@@ -121,7 +121,7 @@ impl Pipeline {
     fn generate_matrix(aspect_ratio: f32) -> UniformBufferObject {
         let mx_projection = cgmath::perspective(cgmath::Deg(45f32), aspect_ratio, 1.0, 10.0);
         let mx_view = cgmath::Matrix4::look_at(
-            cgmath::Point3::new(1.5f32, -5.0, 3.0),
+            cgmath::Point3::new(1.5f32, -0.1, 3.0),
             cgmath::Point3::new(0f32, 0.0, 0.0),
             cgmath::Vector3::unit_z(),
         );
