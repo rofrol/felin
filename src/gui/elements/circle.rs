9ba9adbc3438a10fc0fe149df002cc56c059eed5
tonis2 <tonis.anton18@gmail.com>
2019-08-23 17:27:01 +0300

    Update

--- src/gui/elements/circle.rs
+++ src/gui/elements/circle.rs
@@ -3,22 +3,21 @@ use lyon::tessellation::geometry_builder::{VertexConstructor, VertexBuffers, Buf
 use lyon::tessellation::{StrokeOptions, FillOptions};
 use lyon::tessellation::basic_shapes::*;
 use lyon::tessellation;
-
 use crate::definitions::{Vertex, Element, RenderResult};
 
-
 pub struct Circle {
-    width: f32,
-    height: f32,
+    x: f32,
+    y: f32,
+    radius: f32,
     color: [f32; 3], 
 }
-
 
 impl Circle {
     pub fn new() -> Circle {
         Circle {
-            width: 100.0,
-            height: 100.0,
+            radius: 50.0,
+            x:100.0,
+            y:100.0,
             color: [1.0, 1.0, 1.0],
         }
     }
@@ -31,8 +30,8 @@ impl Circle {
         let fill_options = FillOptions::tolerance(0.01);
 
         fill_circle(
-            point(0.0, 0.0),
-            200.0,
+            point(self.x, self.y),
+            self.radius,
             &fill_options,
             &mut BuffersBuilder::new(&mut mesh, |vertex : tessellation::FillVertex| {
                 Vertex {
