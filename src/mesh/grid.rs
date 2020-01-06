016a0d5279f8fca62b9893291340896001a2f3e7
tonis2 <tonis.anton18@gmail.com>
2020-01-06 18:57:03 +0200

    Update grid rendering

--- src/mesh/grid.rs
+++ src/mesh/grid.rs
@@ -1,6 +1,7 @@
 use crate::definitions::Mesh;
 use crate::prelude::*;
 use crate::utils::{Batch, Style};
+use std::collections::HashMap;
 
 pub struct Grid<'a> {
     pub style: Style,
@@ -20,19 +21,37 @@ impl<'a> Grid<'a> {
     fn calculate_style(
         parent_style: Style,
         children: &mut Vec<&'a mut dyn ElementCore>,
-        batch: &mut Batch,
+        batch: &mut HashMap<String, Batch>,
     ) {
         for child in children.iter_mut() {
             let style = Style::calculate_style(parent_style, child.get_style());
             child.set_style(style);
             child.build();
-
-            batch.add_mesh(&child.mesh());
+            match child.get_id() {
+                // The division was valid
+                Some(id) => {
+                    if batch.contains_key(&id) {
+                        batch.get_mut(&id).unwrap().add_mesh(&child.mesh());
+                    }
+                }
+                // The division was invalid
+                None => batch.get_mut("default").unwrap().add_mesh(&child.mesh()),
+            }
         }
     }
 
-    pub fn into_batch(&mut self) -> Batch {
-        let mut batch = Batch::new();
+    pub fn into_batches(&mut self, filters: Option<Vec<String>>) -> HashMap<String, Batch> {
+        let mut batch: HashMap<String, Batch> = [("default".to_string(), Batch::new())]
+            .iter()
+            .cloned()
+            .collect();
+
+        if filters.is_some() {
+            for item in filters.as_ref().unwrap() {
+                batch.insert(item.to_string(), Batch::new());
+            }
+        }
+
         Self::calculate_style(self.style.clone(), &mut self.children, &mut batch);
         batch
     }
@@ -40,7 +59,6 @@ impl<'a> Grid<'a> {
 
 #[allow(dead_code)]
 impl<'a> ElementCore for Grid<'a> {
-    fn build(&mut self) {}
     fn get_style(&self) -> Style {
         self.style
     }
@@ -48,6 +66,11 @@ impl<'a> ElementCore for Grid<'a> {
         self.style = style;
     }
 
+    fn get_id(&self) -> Option<String> {
+        None
+    }
+
+    fn build(&mut self) {}
     fn mesh(&mut self) -> Mesh {
         Mesh {
             vertices: Vec::new(),
